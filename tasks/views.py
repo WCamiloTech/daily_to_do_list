335e78a72520e1f23eb76270a876fd637d8cbfbb
Williames Camilo <williamescw@gmail.com>
2024-08-05 01:14:46 -0300

    First commit

---
+++ tasks/views.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .models import Task
+
+
+# Create your views here.
+def taskList(request):
+    tasks = Task.objects.all()
+    return render(request, 'tasks/list.html', {'tasks': tasks})
+
+def taskView(request, id):
+    task = get_object_or_404(Task, pk=id)
+    return render(request, 'tasks/task.html', {'task': task})
+
+def helloworld(request):
+    return HttpResponse('Hello World')
+
+def yourName(request, name):
+    return render(request, 'tasks/yourname.html', {'name': name})
