d8bf6aff33b8709e4e957aebb8cc5d23b25fd055
Williames Camilo <williamescw@gmail.com>
2024-08-05 02:32:28 -0300

    Implementação Função Inserir Informações Banco de Dados

--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,31 @@
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
+from .forms import TaskForm
 from .models import Task
 
 
 # Create your views here.
 def taskList(request):
-    tasks = Task.objects.all()
+    tasks = Task.objects.all().order_by('title')
     return render(request, 'tasks/list.html', {'tasks': tasks})
+
 
 def taskView(request, id):
     task = get_object_or_404(Task, pk=id)
     return render(request, 'tasks/task.html', {'task': task})
+
+def newTask(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)  # Corrigido: Criar a instância do formulário com dados POST
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.done = 'doing'
+            task.save()
+            return redirect('/')
+    else:
+        form = TaskForm()  # Corrigido: Usar TaskForm com inicial maiúscula
+    return render(request, 'tasks/addtask.html', {'form': form})
 
 def helloworld(request):
     return HttpResponse('Hello World')
